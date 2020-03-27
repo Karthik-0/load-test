af9e36b69855e2efcff4740da95c355597083a9e
Karthik <karthik@testpress.in>
2020-03-27 22:31:32 +0530

    fix

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
+#!/usr/bin/env bash
+
 sudo apt-get update
 sudo apt-get install build-essential libssl-dev
-
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh -o install_nvm.sh
-
 bash install_nvm.sh
 source ~/.profile
-
 nvm install 10
 sudo apt-get install libx11-xcb1 libxcomposite1 libxi6 libxext6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libasound2 libpangocairo-1.0-0 libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0
