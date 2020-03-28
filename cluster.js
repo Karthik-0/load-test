f0412c0690cd897664aedda6c9c497b35f5beb99
Karthik <karthik@testpress.in>
2020-03-28 15:56:36 +0530

    Run in tabs

--- cluster.js
+++ cluster.js
@@ -4,33 +4,24 @@ const faker = require('faker');
 
 const url = "https://live.testpress.in/b/kar-pad-muz";
 
-async function run () {
-    console.log("Starting test")
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
-    const page = await browser.newPage();
-    const name = faker.name.findName();
-    await page.goto(url);
-    await page.waitForSelector('#\_b_kar-pad-muz_join_name')
-    console.log("Successfully logged as ", name)
-    await page.$eval('#\_b_kar-pad-muz_join_name', (el, value) => el.value = value, name);
-    await page.click('.col-lg-6 > form > .input-group > .input-group-append > .btn')
-    console.log(`${name} Successfully joined in conference`)
-    await page.waitFor(100000)
-    console.log(`${name} left the conference`)
-    browser.close();
-}
-
 (async () => {
     const cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT,
         maxConcurrency: 100,
-        monitor: true,
-        puppeteerOptions: {args: ['--no-sandbox']}
+        puppeteerOptions: {args: ['--no-sandbox'], headless:false}
     });
 
     await cluster.task(async ({ page }) => {
-        run()
-        // Store screenshot, do something else
+        console.log("Starting test")
+        const name = faker.name.findName();
+        await page.goto(url);
+        await page.waitForSelector('#\_b_kar-pad-muz_join_name')
+        console.log("Successfully logged as ", name)
+        await page.$eval('#\_b_kar-pad-muz_join_name', (el, value) => el.value = value, name);
+        await page.click('.col-lg-6 > form > .input-group > .input-group-append > .btn')
+        console.log(`${name} Successfully joined in conference`)
+        await page.waitFor(200000)
+        console.log(`${name} left the conference`)
     });
 
     for (var i=0;i <40; i ++) {
